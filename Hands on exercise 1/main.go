6e8f5e43c0965181c6cd752e5f1aeaa71159d246
Kindness <okeziekindness07@gmail.com>
2026-05-19 16:16:01 +0100

    Update

---
+++ Hands on exercise 1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const pi = 3.14
+
+var radius float64 = 5
+
+func main() {
+	x := 2
+	fmt.Printf("the value of x is %v and the type of x is %T\n", x, x)
+	fmt.Printf("the value of pi is %v and the type of pi is %T\n", pi, pi)
+	fmt.Printf("the value of radius is %v and the type of radius is %T\n", radius, radius)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module Handson
+
+go 1.26.3
+
+require (
+	github.com/kindness07/Dog v0.0.0-20260519001514-2ae8183660b7 // indirect
+	github.com/kindness07/puppy v0.0.0-20260519002623-6e45ba9eeaa7 // indirect
+)
