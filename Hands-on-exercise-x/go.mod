ad4b95cb8deb4d6119c9a44152c40c71e66576bb
Kindness <okeziekindness07@gmail.com>
2026-06-16 15:36:04 +0100

    R

---
+++ Hands-on-exercise-x/go.mod
@@ -0,0 +1,3 @@
+module github.com/kindness07/Hands-on
+
+go 1.26.3
---
+++ Hands-on-exercise-x/main.go
@@ -0,0 +1,21 @@
+package starting_code
+
+import (
+	"fmt"
+
+	"github.com/kindness07/Hands-on/blob/main/Hands%20on%20exercise%20x/dog"
+)
+
+type canine struct {
+	name string
+	age  int
+}
+
+func main() {
+	fido := canine{
+		name: "Fido",
+		age:  dog.Years(10),
+	}
+	fmt.Println(fido)
+	fmt.Println(dog.YearsTwo(20))
+}
